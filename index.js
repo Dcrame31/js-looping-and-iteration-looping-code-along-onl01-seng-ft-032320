becaf4cc1ab9273d5ec4d1df0151448dd3f187c7
Diane Crame <dcrame@icloud.com>
2020-06-22 16:26:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names, greeting) {
+    let array = [];
+    for (let i = 0; i < names.length; i++ ) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${greeting} gift!`);
+}
+  return array;
+}
+
+function countDown(num) {
+    let i = 0;
+      while (num >= i) {
+          console.log(num--);
+      }
+  
+  }
